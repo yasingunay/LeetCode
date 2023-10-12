2be732fca97a96ff6402c9817bf337677ef01abd
Yasin Günay <yasingunaydev@gmail.com>
2023-10-12 15:21:00 +0200

    Update 1480-running-sum-of-1d-array.py

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
@@ -3,7 +3,7 @@ class Solution:
       result =[]
       result.append(nums[0])  # Add the first element of 'nums' to the 'result' list
       for number in nums[1:]:
-        n = result[-1] + number  ##result[-1] refers to the last element in the result list.
+        n = result[-1] + number  #result[-1] refers to the last element in the result list.
         result.append(n)
 
       return result
