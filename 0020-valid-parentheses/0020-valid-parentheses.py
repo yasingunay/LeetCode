6276a740b32f952b52e62d876884e727e3c2f26c
Yasin Günay <yasingunaydev@gmail.com>
2023-11-11 09:01:06 +0100

    Update 0020-valid-parentheses.py

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -2,9 +2,9 @@ class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
         open = ['(', '{', '[']
-        close = [')', '}' , ']' ]
-        pair = {'(':')', '{':'}' , '[': ']' }
-        
+        close = [')', '}', ']']
+        pair = {'(': ')', '{': '}', '[': ']'}
+
         for char in s:
             if char in open:
                 stack.append(char)
@@ -13,3 +13,16 @@ class Solution:
                     return False
                 stack.pop()
         return not stack
+
+def main():
+    solution = Solution()
+
+    # Test cases
+    test_strings = ["()", "[", "()[]{}", "(]", "([)]", "{[]}", "((()))", "((())", "(()))"]
+    
+    for test_str in test_strings:
+        result = solution.isValid(test_str)
+        print(f"Input: {test_str}, Result: {result}")
+
+if __name__ == "__main__":
+    main()
